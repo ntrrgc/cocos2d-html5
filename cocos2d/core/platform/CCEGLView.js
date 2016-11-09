b7ed34dc1f2a45f5d78b3b29685d4e87a0e5dd9d
Alicia Boya García <ntrrgc@gmail.com>
2016-11-09 16:52:31 +0100

    Raise max device pixel ratio to 3

--- cocos2d/core/platform/CCEGLView.js
+++ cocos2d/core/platform/CCEGLView.js
@@ -936,7 +936,7 @@ cc.ContainerStrategy = cc.Class.extend(/** @lends cc.ContainerStrategy# */{
         // Setup pixel ratio for retina display
         var devicePixelRatio = view._devicePixelRatio = 1;
         if (view.isRetinaEnabled())
-            devicePixelRatio = view._devicePixelRatio = Math.min(2, window.devicePixelRatio || 1);
+            devicePixelRatio = view._devicePixelRatio = Math.min(3, window.devicePixelRatio || 1);
         // Setup canvas
         locCanvas.width = w * devicePixelRatio;
         locCanvas.height = h * devicePixelRatio;
